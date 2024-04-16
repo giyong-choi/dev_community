2df904a1138486c0a0168a10514d1014cc773979
giyong-choi <rldyd888@naver.com>
2024-04-16 22:06:09 +0900

    refactor : tinymce 세팅 수정 및 css, html 파일 수정

--- hanwooplz_project/hanwooplz_project/settings.py
+++ hanwooplz_project/hanwooplz_project/settings.py
@@ -161,6 +161,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'hanwooplz_app/static'),
+    os.path.join(BASE_DIR, 'hanwooplz_app/static/js/tinymce'),
 ]
 
 USERIMG_URL = 'user_img/'
